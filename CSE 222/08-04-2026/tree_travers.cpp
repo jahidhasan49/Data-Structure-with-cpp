c46ad9f1a5f8aa27742a48de42b23341f98d8104
MD. JAHID HASAN <jahidcjsq@gmail.com>
2026-04-08 13:22:28 +0600

    Implement binary tree traversal methods

--- CSE 222/08-04-2026/tree_travers.cpp
+++ CSE 222/08-04-2026/tree_travers.cpp
@@ -1,1 +1,80 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+Node* createNode(int val) {
+    Node* newNode = new Node;
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+Node* buildTree(int arr[], int n, int i) {
+    if (i >= n)
+        return NULL;
+
+    Node* root = createNode(arr[i]);
+    root->left = buildTree(arr, n, 2 * i + 1);
+    root->right = buildTree(arr, n, 2 * i + 2);
+
+    return root;
+}
+void preorder(Node* root) {
+    if (root == NULL)
+        return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int* arr = new int[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    Node* root = buildTree(arr, n, 0);
+
+    cout << "Preorder: ";
+    preorder(root);
+
+    cout << "\nInorder: ";
+    inorder(root);
+
+    cout << "\nPostorder: ";
+    postorder(root);
+
+    delete[] arr;
+    return 0;
+}
 
